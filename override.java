9d9c9ab5e0c07f7f391df8d270f1d30cc55a6bdf
Pratheeba S <142966931+Pratheeba21@users.noreply.github.com>
2023-09-01 20:46:45 +0530

    Create override.java
    This java programs implements method override.
    Method overriding in Java is a fundamental concept in object-oriented programming (OOP) that allows a child class(sub class) to provide a specific implementation of a method that is already defined in its parent class(super class)

---
+++ override.java
@@ -0,0 +1,40 @@
+import java.io.*;
+class multiplication
+{
+	int a=8,b=5;
+	int mul;
+	void  mul() 
+	{
+		mul=a*b;
+	}
+	void display()
+	{
+		System.out.println("Multiplication of two integers="+mul);
+	}
+}
+class addition extends multiplication
+{
+	int add;
+	void add()
+	{
+		add=a+b;
+	}
+	void display()
+	{
+		System.out.println("Addition of two integers="+add);
+	}
+}
+class override
+{
+	public static void main(String s[]) throws Exception
+	{
+		multiplication m;
+		addition d=new addition();
+		multiplication n=new multiplication();
+		m=n;
+		m.mul();
+		m.display();
+		d.add();
+		d.display();
+	}
+}
