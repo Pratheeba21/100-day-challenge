8b6ffeeda637827fd63ad186246b63525bbb7db0
Pratheeba S <142966931+Pratheeba21@users.noreply.github.com>
2023-09-03 23:46:00 +0530

    Create si.java

---
+++ si.java
@@ -0,0 +1,10 @@
+// Java program to compute simple interest
+import java.io.*;
+class si {
+	public static void main(String args[])
+	{
+		float P = 1, R = 1, T = 1;
+		float SI = (P * T * R) / 100;
+		System.out.println("Simple interest = " + SI);
+	}
+}
