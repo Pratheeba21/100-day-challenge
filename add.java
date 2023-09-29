e3f69382e698d31bbf9c53c493b5636bbe6590fb
Pratheeba21 <pratheebas887@gmail.com>
2023-09-29 23:10:16 +0530

    java-program

---
+++ add.java
@@ -0,0 +1,17 @@
+class add
+{
+	void ad()
+	{
+		int a;
+		a=2+3;
+		System.out.println(a);
+	}
+}
+class main
+{
+	public static void main(String args[]) throws Exception
+	{
+		add s=new add();
+		s.ad();
+	}
+}
