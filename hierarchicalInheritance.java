e72ae95cbe46a0db13e7194a92997df0616f939f
Pratheeba21 <pratheebas887@gmail.com>
2023-09-30 12:30:24 +0530

    java-program

--- hierarchicalInheritance.java
+++ hierarchicalInheritance.java
@@ -1,24 +1,37 @@
-// Java program to illustrate the
-// concept of Hierarchical inheritance
-
-class A {
-	public void print_A() { System.out.println("Class A"); }
+class A 
+{
+	public void print_A() 
+	{ 
+		System.out.println("Class A");
+	}
 }
 
-class B extends A {
-	public void print_B() { System.out.println("Class B"); }
+class B extends A 
+{
+	public void print_B() 
+	{ 
+		System.out.println("Class B"); 
+	}
 }
 
-class C extends A {
-	public void print_C() { System.out.println("Class C"); }
+class C extends A 
+{
+	public void print_C() 
+	{ 
+		System.out.println("Class C"); 
+	}
 }
 
-class D extends A {
-	public void print_D() { System.out.println("Class D"); }
+class D extends A 
+{
+	public void print_D() 
+	{ 
+		System.out.println("Class D"); 
+	}
 }
 
-// Driver Class
-public class hierarchicalInheritance {
+public class hierarchicalInheritance 
+{
 	public static void main(String[] args)
 	{
 		B obj_B = new B();
