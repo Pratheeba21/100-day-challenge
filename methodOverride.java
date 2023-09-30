ccab9b19e77d604337e08e41bc454349ba6b3d99
Pratheeba21 <pratheebas887@gmail.com>
2023-09-30 12:14:51 +0530

    java-program

--- methodOverride.java
+++ methodOverride.java
@@ -1,53 +1,26 @@
-// Java Program for Method Overriding
-
-// Class 1
-// Helper class
-class methodOverride {
-
-	// Method of parent class
+class methodOverride 
+{
 	void Print()
 	{
-
-		// Print statement
 		System.out.println("parent class");
 	}
 }
-
-// Class 2
-// Helper class
-class subclass1 extends methodOverride {
-
-	// Method
+class subclass1 extends methodOverride
+{
 	void Print() { System.out.println("subclass1"); }
 }
-
-// Class 3
-// Helper class
-class subclass2 extends methodOverride {
-
-	// Method
+class subclass2 extends methodOverride 
+{
 	void Print()
 	{
-
-		// Print statement
 		System.out.println("subclass2");
 	}
 }
-
-// Class 4
-// Main class
-class main {
-
-	// Main driver method
+class main 
+{ 
 	public static void main(String[] args)
 	{
-
-		// Creating object of class 1
 		methodOverride a;
-
-		// Now we will be calling print methods
-		// inside main() method
-
 		a = new subclass1();
 		a.Print();
 
