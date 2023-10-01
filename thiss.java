15da65790f8e9992125353307a63777fdc9cf676
Pratheeba21 <pratheebas887@gmail.com>
2023-10-01 22:49:08 +0530

    java-program

--- thiss.java
+++ thiss.java
@@ -1,42 +1,24 @@
-// Java Program to implement
-// Java this reference
-
-// Driver Class
 public class thiss {
-
-	// Fields Declared
 	String name;
 	int age;
-
-	// Constructor
 	thiss(String name, int age)
 	{
 		this.name = name;
 		this.age = age;
 	}
-
-	// Getter for name
 	public String get_name() { return name; }
-
-	// Setter for name
 	public void change_name(String name)
 	{
 		this.name = name;
 	}
-
-	// Method to Print the Details of
-	// the person
 	public void printDetails()
 	{
 		System.out.println("Name: " + name);
 		System.out.println("Age: " + age);
 		System.out.println();
 	}
-
-	// main function
 	public static void main(String[] args)
 	{
-		// Objects Declared
 		thiss first = new thiss("ABC", 18);
 		thiss second = new thiss("XYZ", 22);
 
