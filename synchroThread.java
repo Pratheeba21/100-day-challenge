95a6ffa6ec7d96836c1738b186227363f875e4d9
Pratheeba21 <pratheebas887@gmail.com>
2023-10-02 18:40:43 +0530

    java-program

--- synchroThread.java
+++ synchroThread.java
@@ -1,43 +1,59 @@
-class threadSynchro extends Thread {
+class threadSynchro extends Thread 
+{
 	public void printThread(int n)
 	{
-		for (int i = 1; i <= 10; i++) {
+		for (int i = 1; i <= 10; i++) 
+		{
 			System.out.println("Thread " + n
 							+ " is working...");
-			try {
+			try 
+			{
 				Thread.sleep(600);
 			}
-			catch (Exception ex) {
+			catch (Exception ex) 
+			{
 				System.out.println(ex.toString());
 			}
 		}
 		System.out.println("--------------------------");
 
-		try {
+		try 
+		{
 			Thread.sleep(1000);
 		}
 
-		catch (Exception ex) {
+		catch (Exception ex) 
+		{
 			System.out.println(ex.toString());
 		}
 	}
 }
-class Thread1 extends Thread {
+class Thread1 extends Thread 
+{
 	threadSynchro test;
-	Thread1(threadSynchro p) { test = p; }
+	Thread1(threadSynchro p) 
+	{ 
+		test = p; 
+	}
 	public void run()
 	{
-
-		// Calling method 1 as in above class
 		test.printThread(1);
 	}
 }
-class Thread2 extends Thread {
+class Thread2 extends Thread 
+{
 	threadSynchro test;
-	Thread2(threadSynchro p) { test = p; }
-	public void run() { test.printThread(2); }
+	Thread2(threadSynchro p) 
+	{ 
+		test = p; 
+	}
+	public void run() 
+	{ 
+		test.printThread(2); 
+	}
 }
-public class synchroThread {
+public class synchroThread 
+{
 	public static void main(String[] args)
 	{
 		threadSynchro p = new threadSynchro();
