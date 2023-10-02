2bd56ed5bbc34b180c1b3f31c9326501e018ec5b
Pratheeba21 <pratheebas887@gmail.com>
2023-10-02 18:39:14 +0530

    java-program

--- synchroThread.java
+++ synchroThread.java
@@ -32,43 +32,18 @@ class Thread1 extends Thread {
 		test.printThread(1);
 	}
 }
-
-// Class 3
-// Helper class extending Thread Class
 class Thread2 extends Thread {
-
-	// Declaring variable of type Class1
 	threadSynchro test;
-
-	// Constructor for class2
 	Thread2(threadSynchro p) { test = p; }
-
-	// run() method of this class for
-	// entry point for thread2
 	public void run() { test.printThread(2); }
 }
-
-// Class 4
-// Main class
 public class synchroThread {
-
-	// Main driver method
 	public static void main(String[] args)
 	{
-
-		// Creating object of class 1 inside main() method
 		threadSynchro p = new threadSynchro();
-
-		// Passing the same object of class PrintTest to
-		// both threads
 		Thread1 t1 = new Thread1(p);
 		Thread2 t2 = new Thread2(p);
-
-		// Start executing the threads
-		// using start() method
 		t1.start();
 		t2.start();
-
-		// This will print both the threads simultaneously
 	}
 }
