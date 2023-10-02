a8b51cec70d5391031fadb6bb95a79cf04454dba
Pratheeba21 <pratheebas887@gmail.com>
2023-10-02 18:38:11 +0530

    java-program

--- synchroThread.java
+++ synchroThread.java
@@ -1,68 +1,30 @@
-// Class 1
-// Helper class
-// Extending Thread class
 class threadSynchro extends Thread {
-
-	// Non synchronized Code
-
-	// Method 1
 	public void printThread(int n)
 	{
-
-		// This loop will print the currently executed
-		// thread
 		for (int i = 1; i <= 10; i++) {
 			System.out.println("Thread " + n
 							+ " is working...");
-
-			// Try block to check for exceptions
 			try {
-
-				// Pause the execution of current thread
-				// for 0.600 seconds using sleep() method
 				Thread.sleep(600);
 			}
-
-			// Catch block to handle the exceptions
 			catch (Exception ex) {
-
-				// Overriding existing toString() method and
-				// prints exception if occur
 				System.out.println(ex.toString());
 			}
 		}
-
-		// Display message for better readability
 		System.out.println("--------------------------");
 
 		try {
-
-			// Pause the execution of current thread
-			// for 0.1000 millisecond or 1sec using sleep
-			// method
 			Thread.sleep(1000);
 		}
 
 		catch (Exception ex) {
-
-			// Printing the exception
 			System.out.println(ex.toString());
 		}
 	}
 }
-
-// Class 2
-// Helper class extending Thread Class
 class Thread1 extends Thread {
-
-	// Declaring variable of type Class1
 	threadSynchro test;
-
-	// Constructor for class1
 	Thread1(threadSynchro p) { test = p; }
-
-	// run() method of this class for
-	// entry point for thread1
 	public void run()
 	{
 
