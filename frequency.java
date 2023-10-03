3920bbf90032d3191ef4dc9208116ea4ef50195e
Pratheeba21 <pratheebas887@gmail.com>
2023-10-03 22:19:14 +0530

    java-program

--- frequency.java
+++ frequency.java
@@ -1,8 +1,6 @@
 public class frequency {  
-    public static void main(String[] args) {  
-        //Initialize array  
-        int [] arr = new int [] {1, 2, 8, 3, 2, 2, 2, 5, 1};  
-        //Array fr will store frequencies of element  
+    public static void main(String[] args) {    
+        int [] arr = new int [] {1, 2, 8, 3, 2, 2, 2, 5, 1};   
         int [] fr = new int [arr.length];  
         int visited = -1;  
         for(int i = 0; i < arr.length; i++){  
@@ -17,8 +15,6 @@ public class frequency {
             if(fr[i] != visited)  
                 fr[i] = count;  
         }  
-  
-        //Displays the frequency of each element present in array  
         System.out.println("---------------------------------------");  
         System.out.println(" Element | Frequency");  
         System.out.println("---------------------------------------");  
