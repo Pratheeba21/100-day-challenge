3c16dda1929590bd42c9ba1624f08a4c68c45ea8
Pratheeba21 <pratheebas887@gmail.com>
2023-10-03 22:20:08 +0530

    java-program

--- frequency.java
+++ frequency.java
@@ -1,12 +1,17 @@
-public class frequency {  
-    public static void main(String[] args) {    
+public class frequency 
+{  
+    public static void main(String[] args) 
+    {    
         int [] arr = new int [] {1, 2, 8, 3, 2, 2, 2, 5, 1};   
         int [] fr = new int [arr.length];  
         int visited = -1;  
-        for(int i = 0; i < arr.length; i++){  
+        for(int i = 0; i < arr.length; i++)
+        {  
             int count = 1;  
-            for(int j = i+1; j < arr.length; j++){  
-                if(arr[i] == arr[j]){  
+            for(int j = i+1; j < arr.length; j++)
+            {  
+                if(arr[i] == arr[j])
+                {  
                     count++;  
                     //To avoid counting same element again  
                     fr[j] = visited;  
