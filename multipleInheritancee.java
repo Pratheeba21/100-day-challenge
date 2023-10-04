8b2032a5ae5407c4bf0c806daa8cd5c565607f1d
Pratheeba21 <pratheebas887@gmail.com>
2023-10-04 21:42:45 +0530

    java-program

--- multipleInheritancee.java
+++ multipleInheritancee.java
@@ -1,6 +1,4 @@
 interface Backend {
-
-  // abstract class
   public void connectServer();
 }
 
@@ -10,26 +8,17 @@ class Frontend {
     System.out.println(str + " can also be used as frontend.");
   }
 }
-
-// Language extends Frontend class
-// Language implements Backend interface
 class multipleInheritancee extends Frontend implements Backend {
 
   String language = "Java";
-
-  // implement method of interface
   public void connectServer() {
     System.out.println(language + " can be used as backend language.");
   }
 
   public static void main(String[] args) {
-
-    // create object of Language class
      multipleInheritancee java = new multipleInheritancee();
 
     java.connectServer();
-
-    // call the inherited method of Frontend class
     java.responsive(java.language);
   }
 
