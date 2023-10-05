70e51e0657729672bf01adbb7ebd794be0510fad
Pratheeba21 <pratheebas887@gmail.com>
2023-10-05 22:24:50 +0530

    java-program

--- oops.java
+++ oops.java
@@ -1,67 +1,53 @@
 class Shape 
 {
     private String color;
-
     public Shape(String color) 
     {
         this.color = color;
     }
-
     public void setColor(String color) 
     {
         this.color = color;
     }
-
     public String getColor() 
     {
         return color;
     }
-
     public double getArea() 
     {
         return 0.0;
     }
 }
-
-
 class Circle extends Shape 
 {
     private double radius;
-
     public Circle(String color, double radius) 
     {
         super(color);
         this.radius = radius;
     }
-
     @Override
     public double getArea() 
     {
         return Math.PI * radius * radius;
     }
 }
-
-
 class Rectangle extends Shape 
 {
     private double length;
     private double width;
-
     public Rectangle(String color, double length, double width) 
     {
         super(color);
         this.length = length;
         this.width = width;
     }
-
     @Override
     public double getArea() 
     {
         return length * width;
     }
 }
-
-
 public class oops
 {
     public static void main(String[] args) 
