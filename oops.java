86252d5c719534b8c3dafacd45428d0667205605
Pratheeba21 <pratheebas887@gmail.com>
2023-10-05 22:23:15 +0530

    java-program

--- oops.java
+++ oops.java
@@ -1,59 +1,71 @@
-
-class Shape {
+class Shape 
+{
     private String color;
 
-    public Shape(String color) {
+    public Shape(String color) 
+    {
         this.color = color;
     }
 
-    public void setColor(String color) {
+    public void setColor(String color) 
+    {
         this.color = color;
     }
 
-    public String getColor() {
+    public String getColor() 
+    {
         return color;
     }
 
-    public double getArea() {
+    public double getArea() 
+    {
         return 0.0;
     }
 }
 
 
-class Circle extends Shape {
+class Circle extends Shape 
+{
     private double radius;
 
-    public Circle(String color, double radius) {
+    public Circle(String color, double radius) 
+    {
         super(color);
         this.radius = radius;
     }
 
     @Override
-    public double getArea() {
+    public double getArea() 
+    {
         return Math.PI * radius * radius;
     }
 }
 
 
-class Rectangle extends Shape {
+class Rectangle extends Shape 
+{
     private double length;
     private double width;
 
-    public Rectangle(String color, double length, double width) {
+    public Rectangle(String color, double length, double width) 
+    {
         super(color);
         this.length = length;
         this.width = width;
     }
 
     @Override
-    public double getArea() {
+    public double getArea() 
+    {
         return length * width;
     }
 }
 
 
-public class oops{
-    public static void main(String[] args) {
+public class oops
+{
+    public static void main(String[] args) 
+    {
         Circle circle = new Circle("Red", 5.0);
         Rectangle rectangle = new Rectangle("Blue", 4.0, 6.0);
 
