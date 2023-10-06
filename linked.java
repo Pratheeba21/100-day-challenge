cabd84060266039d018a69d358947bab4554a633
Pratheeba21 <pratheebas887@gmail.com>
2023-10-06 21:52:56 +0530

    java-program

--- linked.java
+++ linked.java
@@ -2,12 +2,8 @@ import java.util.Arrays;
 import java.util.LinkedList; 
 public class linked 
 { 
-
-	// Function to convert LinkedList to Array 
 	public static <T> Object[] convertLinkedListToArray(LinkedList<T> linkedList) 
 	{ 
-
-		// Converting LinkedList to Array 
 		Object[] array = linkedList.toArray(); 
 
 		return array; 
@@ -15,29 +11,19 @@ public class linked
 
 	public static void main(String args[]) 
 	{ 
-		// Creating linked list 
 		LinkedList<String> 
 			linkedList = new LinkedList<String>(); 
-
-		// Adding elements to the linked list 
 		linkedList.add("g"); 
 		linkedList.add("r"); 
 		linkedList.add("e"); 
 		linkedList.add("e"); 
 		linkedList.add("n"); 
-
-		// Print the LinkedList 
 		System.out.println("Linked list: "
 						+ linkedList); 
-
-		// Converting LinkedList to Object Array 
 		Object[] objArray = convertLinkedListToArray(linkedList); 
-
-		// Convert Object[] to String[] 
 		String[] array = Arrays.copyOf(objArray, 
 									objArray.length, 
 									String[].class); 
-		// Print the String Array 
 		System.out.println("Array: "
 						+ Arrays.toString(array)); 
 	} 
