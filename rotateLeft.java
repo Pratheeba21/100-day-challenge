41e9a731c8b4a6e2ffc8deda7040d408db684fe9
Pratheeba S <142966931+Pratheeba21@users.noreply.github.com>
2023-10-08 23:00:00 +0530

    java-program

--- rotateLeft.java
+++ rotateLeft.java
@@ -1,20 +1,25 @@
-class rotateLeft {  
-    public static void main(String[] args) {  
+class rotateLeft 
+{  
+    public static void main(String[] args) 
+    {  
         //Initialize array  
         int [] arr = new int [] {1, 2, 3, 4, 5};  
         //n determine the number of times an array should be rotated  
         int n = 3;  
         //Displays original array  
         System.out.println("Original array: ");  
-        for (int i = 0; i < arr.length; i++) {  
+        for (int i = 0; i < arr.length; i++) 
+        {  
             System.out.print(arr[i] + " ");  
         }  
         //Rotate the given array by n times toward left  
-        for(int i = 0; i < n; i++){  
+        for(int i = 0; i < n; i++)
+        {  
             int j, first;  
             //Stores the first element of the array  
             first = arr[0];  
-            for(j = 0; j < arr.length-1; j++){  
+            for(j = 0; j < arr.length-1; j++)
+            {  
                 //Shift element of array by one  
                 arr[j] = arr[j+1];  
             }  
@@ -24,7 +29,8 @@ class rotateLeft {
         System.out.println();  
         //Displays resulting array after rotation  
         System.out.println("Array after left rotation: ");  
-        for(int i = 0; i< arr.length; i++){  
+        for(int i = 0; i< arr.length; i++)
+        {  
             System.out.print(arr[i] + " ");  
         }  
     }  
