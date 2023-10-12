b601bc86389dae0c507992e042e2805f3961a66b
Pratheeba21 <pratheebas887@gmail.com>
2023-10-12 22:03:53 +0530

    java-program

--- countNodes.java
+++ countNodes.java
@@ -1,84 +1,70 @@
-public class countNodes {    
-        
-    //Represent a node of singly linked list    
-    class Node{    
+public class countNodes 
+{    
+    class Node
+    {    
         int data;    
         Node next;    
             
-        public Node(int data) {    
+        public Node(int data) 
+        {    
             this.data = data;    
             this.next = null;    
         }    
-    }    
-     
-    //Represent the head and tail of the singly linked list    
+    }       
     public Node head = null;    
     public Node tail = null;    
-        
-    //addNode() will add a new node to the list    
-    public void addNode(int data) {    
-        //Create a new node    
+    public void addNode(int data) 
+    {     
         Node newNode = new Node(data);    
-            
-        //Checks if the list is empty    
-        if(head == null) {    
-            //If list is empty, both head and tail will point to new node    
+        if(head == null) 
+        {        
             head = newNode;    
             tail = newNode;    
         }    
-        else {    
-            //newNode will be added after tail such that tail's next will point to newNode    
-            tail.next = newNode;    
-            //newNode will become new tail of the list    
+        else 
+        {       
+            tail.next = newNode;       
             tail = newNode;    
         }    
-    }    
-        
-    //countNodes() will count the nodes present in the list    
-    public int countNodes() {    
-        int count = 0;    
-        //Node current will point to head    
+    }      
+    public int countNodes() 
+    {    
+        int count = 0;     
         Node current = head;    
             
-        while(current != null) {    
-            //Increment the count by 1 for each node    
+        while(current != null) 
+        {     
             count++;    
             current = current.next;    
         }    
         return count;    
-    }    
-            
-    //display() will display all the nodes present in the list    
-    public void display() {    
-        //Node current will point to head    
+    }        
+    public void display() 
+    {       
         Node current = head;    
-        if(head == null) {    
+        if(head == null) 
+        {    
             System.out.println("List is empty");    
             return;    
         }    
         System.out.println("Nodes of singly linked list: ");    
-        while(current != null) {    
-            //Prints each node by incrementing pointer    
+        while(current != null) 
+        {       
             System.out.print(current.data + " ");    
             current = current.next;    
         }    
         System.out.println();    
     }    
         
-    public static void main(String[] args) {    
+    public static void main(String[] args) 
+    {    
             
-        countNodes sList = new countNodes();    
-            
-        //Add nodes to the list    
+        countNodes sList = new countNodes();      
         sList.addNode(1);    
         sList.addNode(2);    
         sList.addNode(3);    
-        sList.addNode(4);    
-            
-        //Displays the nodes present in the list    
-        sList.display();    
-            
-        //Counts the nodes present in the given list    
+        sList.addNode(4);     
+        sList.display();      
         System.out.println("Count of nodes present in the list: " + sList.countNodes());    
     }    
 }    
