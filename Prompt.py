05830404207964442b1f4affc58888dc691ac8dd
Pratheeba S <142966931+Pratheeba21@users.noreply.github.com>
2024-07-15 15:16:16 +0530

    Create Prompt.py

---
+++ Prompt.py
@@ -0,0 +1,37 @@
+ChatGPT
+
+You need to generate a python program to run using django with the following requirements:
+1st - Create a registration form with fields username,e-mail id and password 
+2nd - the registration form should contain 
+--one check box for show password/hide password 
+--register button 
+3rd - when the user first time register and click the register button it should store the values in the database (the default in django db  sqlite3)
+--and display a message as you have successfully registered 
+4th - Create a login form with fields for username and password and a login button 
+5th - the login form should contain
+--an option for forgot password 
+--When the user click the forgot password ,it should be directing them to a change password page 
+--that change password page should contain two fields like new password and confirm password 
+--then the new password is updated in the database 
+--one check box for show password/hide password 
+6th - when the user login by clicking the login button 
+--it should get the username and password 
+--then validate if the username is matching with the corresponding password or not with the help of the datas stored in the database 
+7th - if the username and password matches then it should display a message like you have successfully logged in 
+8th - if the username and password does not match then it should display a message like incorrect username or password .
+----The above all are my requirements---
+[-I am new to Django
+-i have created a folder for my project on my desktop 
+-i have create a virtual environment for the project through git bash cmd
+-i have activated my virtual environment 
+-i have successfully created my django project and opened it in vs code
+-now i have two folders in my project folder which I have created on my desktop 
+- those are, virtual environment folder and project name folder
+--now i have 5 python files in my project folder those are
+ 1.__init__.py
+2.settings.py
+3.asgi.py
+4.wsgi.py
+5.urls.py
+- now I need you to give me the code which will be implemented on the above environment 
+-give me clear instructions for which code to implement in which file and all]
